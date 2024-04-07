c4ddf33667ebe1f6a5c860d4dd62fc43608e3d42
emmalaura <108916939+emmalaura@users.noreply.github.com>
2024-04-07 12:57:17 -0500

    Create htree.c

---
+++ htree.c
@@ -0,0 +1,87 @@
+
+/*
+Emma Gonzalez NetID: elg210004
+Mateo Estrada NetID: mxe210022
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <errno.h>     // for EINTR
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+// Print out the usage of the program and exit.
+void Usage(char*);
+uint32_t jenkins_one_at_a_time_hash(const uint8_t* , uint64_t );
+
+// block size
+#define BSIZE 4096
+
+int
+main(int argc, char** argv)
+{
+  int32_t fd;
+  uint32_t nblocks;
+
+  // input checking
+  if (argc != 3)
+    Usage(argv[0]);
+
+  // open input file
+  fd = open(argv[1], O_RDWR);
+  if (fd == -1) {
+    perror("open failed");
+    exit(EXIT_FAILURE);
+  }
+  // use fstat to get file size
+  // calculate nblocks
+
+  printf(" no. of blocks = %u \n", nblocks);
+
+  double start = GetTime();
+
+  // calculate hash value of the input file
+
+
+  double end = GetTime();
+  printf("hash value = %u \n", hash);
+  printf("time taken = %f \n", (end - start));
+  close(fd);
+  return EXIT_SUCCESS;
+}
+
+uint32_t
+jenkins_one_at_a_time_hash(const uint8_t* key, uint64_t length)
+{
+  unint64_t i = 0;
+  uint32_t hash = 0;
+
+  while (i != length) {
+    hash += key[i++];
+    hash += hash << 10;
+    hash ^= hash >> 6;
+  }
+  hash += hash << 3;
+  hash ^= hash >> 11;
+  hash += hash << 15;
+  return hash;
+}
+
+
+void
+Usage(char* s)
+{
+  fprintf(stderr, "Usage: %s filename num_threads \n", s);
+  exit(EXIT_FAILURE);
+}
+
+
+
+
+
+
