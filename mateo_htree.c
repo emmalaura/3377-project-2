818a48507aace5fb8e7d723b1c1d60230c33d2e1
Mateo Estrada <mateo404521@gmail.com>
2024-04-13 22:16:30 -0500

    pushing personal code

---
+++ mateo_htree.c
@@ -0,0 +1,101 @@
+/*
+Emma Gonzalez NetID: elg210004
+Mateo Estrada NetID: mxe210022
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <errno.h>     // for EINTR
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+// Print out the usage of the program and exit.
+void Usage(char*);
+uint32_t jenkins_one_at_a_time_hash(const uint8_t* , uint64_t );
+
+// block size
+#define BSIZE 4096
+
+int
+main(int argc, char** argv)
+{
+  int32_t fd;
+  uint32_t nblocks;
+
+  // input checking
+  if (argc != 3)
+    Usage(argv[0]);
+
+  // open input file
+  fd = open(argv[1], O_RDWR);
+  if (fd == -1) {
+    perror("open failed");
+    exit(EXIT_FAILURE);
+  }
+  // use fstat to get file size
+  // calculate nblocks
+  struct stat statbuf;
+  // use fstat to get file size
+  if(fstat(fd, &statbuf) == -1){
+    perror("fstat failed");
+    exit(EXIT_FAILURE);
+  }
+  size_t size = statbuf.st_size;
+  nblocks = size / BSIZE;
+  printf(" no. of blocks = %u \n", nblocks);
+
+  //double start = GetTime();
+
+  // calculate hash value of the input file
+  void* arr = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
+  if (arr == MAP_FAILED) {
+    perror("mmap failed");
+    exit(EXIT_FAILURE);
+  }
+
+
+  //double end = GetTime();
+  //printf("hash value = %u \n", hash);
+  //printf("time taken = %f \n", (end - start));
+  close(fd);
+  return EXIT_SUCCESS;
+}
+
+uint32_t
+jenkins_one_at_a_time_hash(const uint8_t* key, uint64_t length)
+{
+  uint64_t i = 0;
+  uint32_t hash = 0;
+
+  while (i != length) {
+    hash += key[i++];
+    hash += hash << 10;
+    hash ^= hash >> 6;
+  }
+  hash += hash << 3;
+  hash ^= hash >> 11;
+  hash += hash << 15;
+  return hash;
+}
+
+
+void
+Usage(char* s)
+{
+  fprintf(stderr, "Usage: %s filename num_threads \n", s);
+  exit(EXIT_FAILURE);
+}
+
+
+
+
+
+
