067fb65ebe5ec77af4b551de701db21794885bc1
Mateo Estrada <mateo404521@gmail.com>
2024-04-19 16:41:09 -0500

    mateo_htree.c

--- mateo_htree.c
+++ mateo_htree.c
@@ -1,8 +1,3 @@
-/*
-Emma Gonzalez NetID: elg210004
-Mateo Estrada NetID: mxe210022
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -30,6 +25,7 @@ int chunk_size = 0;
 int32_t fd = 0;
 char *buffer = NULL;
 uint64_t fileSize = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; // Initialize mutex
 
 #define BSIZE 4096
 
@@ -56,7 +52,7 @@ int main(int argc, char *argv[]) {
     uint64_t fileSize = fileInfo.st_size;
 
     // Map file to memory
-    void* arr = mmap(NULL, fileSize, PROT_READ, MAP_PRIVATE, fd, 0);
+    void* arr = mmap(NULL, fileSize, PROT_READ, MAP_SHARED, fd, 0);
     if(arr == MAP_FAILED) {
       perror("mmap failed.");
       exit(EXIT_FAILURE);
@@ -80,6 +76,8 @@ int main(int argc, char *argv[]) {
     double end = GetTime();
 
     // Print results
+
+    printf("num Threads = %d\n", num_threads);
     printf("hash value = %u\n", hash);
     printf("time taken = %f\n", (end - start));
 
@@ -106,46 +104,42 @@ uint32_t jenkins_one_at_a_time_hash(const uint8_t *key, uint64_t length) {
 }
 
 void *child(void *ptr) {
-  uint32_t hash;
   uint32_t n = (uint32_t)(uintptr_t)ptr;
+  uint32_t hash = jenkins_one_at_a_time_hash((uint8_t *)&buffer[n * chunk_size], chunk_size);
   pthread_t thread1, thread2;
-
+  uint32_t ptr1, ptr2;
   bool one_child= false, two_children = false;
+
+
   if (2 * n + 1 < num_threads) {
       pthread_create(&thread1, NULL, child, (void *)(uintptr_t)(2 * n + 1));
-      one_child = true;
+      pthread_join(thread1, (void **)&ptr1);
+         one_child = true;
       if (2 * n + 2 < num_threads) {
-          pthread_create(&thread2, NULL, child, (void *)(uintptr_t)(2 * n + 2));
-          two_children = true;
+      pthread_create(&thread2, NULL, child, (void *)(uintptr_t)(2 * n + 2));
+      pthread_join(thread2, (void **)&ptr2);
+      two_children = true;
       }
   }
 
   char temp_buffer[1000];
 
-  hash = jenkins_one_at_a_time_hash((uint8_t *)&buffer[n * chunk_size], chunk_size);
+    pthread_mutex_lock(&mutex);
 
-  uint32_t ptr1, ptr2;
+    // Concatenate hash values and calculate hash of the concatenated string
+    if (two_children) {
+        sprintf(temp_buffer, "%u%u%u", hash, ptr1, ptr2);
+        hash = (uint32_t)jenkins_one_at_a_time_hash((uint8_t *)temp_buffer, strlen(temp_buffer));
+    } else if (one_child) {
+        sprintf(temp_buffer, "%u%u", hash, ptr1);
+        hash = jenkins_one_at_a_time_hash((uint8_t *)temp_buffer, strlen(temp_buffer));
+    }
 
-  // Join child threads and concatenate hash values
-  if (one_child) {
-      pthread_join(thread1, (void *)&ptr1);
-  }
-  if (two_children) {
-      pthread_join(thread2, (void *)&ptr2);
-  }
-
-  // Concatenate hash values and calculate hash of the concatenated string
-  if (two_children) {
-      sprintf(temp_buffer, "%u%u%u", hash, ptr1, ptr2);
-      hash = (uint32_t)jenkins_one_at_a_time_hash((uint8_t *)temp_buffer, strlen(temp_buffer));
-  } else if (one_child) {
-      sprintf(temp_buffer, "%u%u", hash, ptr1);
-      hash = jenkins_one_at_a_time_hash((uint8_t *)temp_buffer, strlen(temp_buffer));
-  }
-
+    // Unlock mutex after modifying shared variables
+    pthread_mutex_unlock(&mutex);
 
   // Exit the thread and return the hash value
-  return (void *)(uintptr_t)hash;
+  pthread_exit((void *)(uintptr_t)hash);
 }
 
 void usage(char *s) {
